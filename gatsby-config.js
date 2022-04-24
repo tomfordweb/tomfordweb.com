ad8d298cb2eb4e955612caa0e8a3e87e0ee42a63
Tom Ford <tomfordweb@gmail.com>
2022-04-23 22:46:42 -0400

    add gtm

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,5 +44,19 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ["G-8SK004XX4Q"],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
   ],
 };
