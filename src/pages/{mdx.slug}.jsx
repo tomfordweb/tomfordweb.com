940fced11e720644975b1acff84a995556d5800c
Tom Ford <tom@datacompusa.com>
2025-08-30 12:04:36 -0400

    gruvbox styling

--- src/pages/{mdx.slug}.jsx
+++ src/pages/{mdx.slug}.jsx
@@ -3,18 +3,26 @@ import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../components/layout";
 import { Helmet } from "react-helmet";
+import AboutContent from "../components/about-content";
+import { defineCustomElements as deckDeckGoHighlightElement } from "@deckdeckgo/highlight-code/dist/loader";
+
+// markup
+deckDeckGoHighlightElement();
 
 const BlogPost = ({ data }) => {
   return (
-    <Layout pageClass="BlogPost container">
+    <Layout pageName="BlogPost">
       <Helmet>
         <title>{data.mdx.frontmatter.title} - tomfordweb.com</title>
       </Helmet>
       <header className="mb-5">
-        <h1 className="text-italic">{data.mdx.frontmatter.title}</h1>
+        <h1 className="fira-code-bold">{data.mdx.frontmatter.title}</h1>
         <time>{data.mdx.frontmatter.date}</time>
       </header>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
+      <footer className="container text-center d-flex align-items-center">
+        <AboutContent />
+      </footer>
     </Layout>
   );
 };
